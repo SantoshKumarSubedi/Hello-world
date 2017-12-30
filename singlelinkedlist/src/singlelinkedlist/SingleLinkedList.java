d7a732f6620baee87b4d30224ddb704cc4e7135e
Santosh Kumar Subedi <subedisantos9@gmail.com>
2017-12-30 08:17:44 +0545

    completed

--- singlelinkedlist/src/singlelinkedlist/SingleLinkedList.java
+++ singlelinkedlist/src/singlelinkedlist/SingleLinkedList.java
@@ -1,9 +1,78 @@
 package singlelinkedlist;
 
-public class SingleLinkedList {
+public class SingleLinkedList<E> {
+	 private static class Node<E>{
+		 private E element;
+		 private Node<E> next;
+		 
+		 public Node(E e,Node<E> n) {
+			 element=e;
+			 next=n;
+		 }
+		 
+		 public E getElement() {
+			 return element;
+		 }
+		 
+		 public Node<E> getNext(){
+			 return next;
+		 }
+		 public void setNext(Node<E> n) {
+			 next=n;
+		 }
+	 }
 	 
-	public static void main(String Aug[]) {
-		System.out.println("santoshkumarsubedi");
+	 private Node<E> head=null;
+	 private Node<E> tail=null;
+	 private int size=0;
+	 public SingleLinkedList() {}
+		 public int size() {return size;}
+		 public boolean isEmpty() {return size==0;}
+		 public E first() {
+			 if(isEmpty())return null;
+			 return head.getElement();
+		 }
+		 public E last() {
+			 if(isEmpty())return null;
+			 return tail.getElement();
+		 }
+		 
+		 public void addFirst(E e) {
+			 head=new Node<>(e,head);
+			 if(size==0){
+				 tail=head;
+				 size++;
+			 }
+		 }
+		 
+		 public void addLast(E e) {
+			 Node<E> newest=new Node<>(e,null);
+			 if(isEmpty()) {
+				 head=newest;
+			 }else {
+				 tail.setNext(newest);
+				 tail=newest;
+				 size++;
+			 }
+			 
+		 }
+		 
+		 public E removeFirst() {
+			 if(isEmpty())return null;
+			 E answer=head.getElement();
+			 head=head.getNext();
+			 size--;
+			 if(size==0)
+				 tail=null;
+			 return answer;
+		 }
+	 
+	public static void main(String[] args) {
+		SingleLinkedList<Integer> a=new SingleLinkedList<>();
+		a.addFirst(1);
+		a.addLast(4);
+		System.out.println(a.last());
+		a.addFirst(5);
 	}
-dsaaffsdfasdf
+
 }
